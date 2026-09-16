7c738883249fd2791df367d53217518bd32cd6f1
Maria Fominova <murashka2010@gmail.com>
2026-09-16 21:08:59 +0300

    Chapter 12/2. bugfix

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -26,7 +26,10 @@ export type State = {
   message?: string | null;
 };
  
-export async function createInvoice(formData: FormData) {
+export async function createInvoice(
+  prevState: State,
+  formData: FormData,
+): Promise<State> {
     const { customerId, amount, status } = CreateInvoice.parse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
